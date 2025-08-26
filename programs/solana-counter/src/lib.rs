85b346d0c00063efbc86c54e3d0dbb32c2601eec
maomao <2892798998@qq.com>
2025-08-26 10:34:35 +0800

    init

---
+++ programs/solana-counter/src/lib.rs
@@ -0,0 +1,44 @@
+use anchor_lang::prelude::*;
+
+declare_id!("By1LmF3KvZYupqCdEqzqfRx1HFAyEUEj3k796ritfdgi");
+
+#[program]
+pub mod solana_counter {
+    use super::*;
+
+    pub fn initialize(ctx: Context<InitializeCounter>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        let counter = &mut ctx.accounts.counter;
+        counter.count = 0;
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<IncrementCounter>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count = counter.count.checked_add(1).unwrap();
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct InitializeCounter<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(init, payer = signer, space = 8 + 8)] // 8字节用于账户头 + 8字节用于u64
+    pub counter: Account<'info, Counter>, // Account 用来访问 本 Program 自己管理的数据账户
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct IncrementCounter<'info> {
+    #[account(mut)]
+    pub counter: Account<'info, Counter>,
+}
+
+#[account]
+pub struct Counter {
+    pub count: u64
+}
+
+// Account 用来访问 本 Program 自己管理的数据账户
+// InterfaceAccount 用来访问 外部 Program（比如 SPL Token Program）定义的账户
---
+++ tests/solana-counter.ts
@@ -0,0 +1,55 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { SolanaCounter } from "../target/types/solana_counter";
+import { Keypair } from "@solana/web3.js";
+import { assert } from "chai";
+
+describe("solana-counter", () => {
+  // 配置本地 provider
+  const provider = anchor.AnchorProvider.local();
+  anchor.setProvider(provider);
+
+    // SolanaCounter对应Anchor.toml里programs.localnet.solana_counter的PascalCase ！！！
+    const program = anchor.workspace.SolanaCounter as Program<SolanaCounter>;
+
+  // 计数器账户 keypair
+  const counter = Keypair.generate();
+
+  it("初始化计数器", async () => {
+    await program.methods.initialize()
+        .accounts({
+          counter: counter.publicKey,
+          signer: provider.wallet.publicKey,
+        })
+        .signers([counter]) // counter 是新建的账户，必须签名
+        .rpc();
+
+    const account = await program.account.counter.fetch(counter.publicKey);
+    assert.equal(account.count.toNumber(), 0);
+    console.log("初始化完成，count =", account.count.toNumber());
+  });
+
+  it("执行 increment (+1)", async () => {
+    await program.methods.increment()
+        .accounts({
+          counter: counter.publicKey,
+        })
+        .rpc();
+
+    const account = await program.account.counter.fetch(counter.publicKey);
+    assert.equal(account.count.toNumber(), 1);
+    console.log("执行 increment 后，count =", account.count.toNumber());
+  });
+});
+
+// output:
+//   solana-counter
+// 初始化完成，count = 0
+//     ✔ 初始化计数器 (528ms)
+// 执行 increment 后，count = 1
+//     ✔ 执行 increment (+1) (480ms)
+//
+//
+//   2 passing (1s)
+//
+// ✨  Done in 2.18s.
